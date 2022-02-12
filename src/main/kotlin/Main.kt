bdfa6493152c0b47248000e76db5152c19badb02
Angara-Marie <angaramarie@gmail.com>
2022-02-12 14:32:39 +0300

    Assignment1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var name = "Tessa Marie"
+    println(name)
+    var age: Byte = 25
+    println(age)
+    var phonenumber: Long = 254716507709
+    println(phonenumber)
+    var weightinKg : Byte = 68
+    println(weightinKg)
+    val Citizen = "Kenyan"
+    println(Citizen)
+}
